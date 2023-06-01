6c76a06b33aa33bde21dea8079fd0f4c8ec95e29
nicksonpeter <nickrocky5200@gmail.com>
2023-06-01 10:28:47 +0000

    adding files

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -6,7 +6,10 @@ import sys
 
 def fizzbuzz(n):
     """
-    printing "Buzz" and "FizzBuzz"
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
